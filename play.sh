3c62d8aaa8780eb3dd9a68fb58b6f9503f886a7a
TilTNeXus <395skr@gmail.com>
2021-03-02 07:43:56 +0000

    Update play.sh

--- play.sh
+++ play.sh
@@ -5,16 +5,16 @@ hour=`date +"%I"`
 if [ `date +"%M"` -lt 10 ];
 then # 1-9
     minFirst=00
-    minSecond=`date +"%M" | sed -e 's/^.*\(.\)$/\1/'`
+    minSecond=`date +"%M" | cut -c2`
 elif [ `date +"%M"` -eq 0 ];
 then # o clock
     minFirst=0
     minSecond=0
 elif [ `date +"%M"` -gt 19 ];
 then # 20-60
-    minFirst=`date +"%M" | sed 's/\B\w*//g;s/\s//g'`
+    minFirst=`date +"%M" | cut -c1`
     minFirst=`echo $minFirst\0`
-    minSecond=`date +"%M" | sed -e 's/^.*\(.\)$/\1/'`
+    minSecond=`date +"%M" | cut -c2`
 else # 10-19
     minFirst=`date +"%M"`
     minSecond=0
